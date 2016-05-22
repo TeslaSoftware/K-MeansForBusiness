d0e671206a9f1bf53d6b3ba2f140080280f9aea9
David <david.tzvi.rosen@gmail.com>
2016-05-22 14:28:51 -0400

    Update KmeansGUI.java

--- KmeansGUI.java
+++ KmeansGUI.java
@@ -1,6 +1,5 @@
 /*
  * KmeansGUI.java
- * This class allows user to select from check boxes the targeted age group 
  * then user can select from dropdown menu number of k (from 2 to lets say 100) 
  * then user can select from dropown menu targeted age group 
  * Then user presses run button after which algorithm is run
@@ -8,10 +7,19 @@
  * <Optional> Then user will get displayed google maps showing geo-location of each centroid
  * you can find this useful for mapping multiple data points on a map https://gist.github.com/parth1020/4481893
  */
-public class KmeansGUI {
-
+import javax.swing.*;
+public class KmeansGUI extends JFrame{
+	static JMenuBar menuBar = new JMenuBar();//menu bar used to hold the GUI's drop down menus
 	
-	
+	public KmeansGUI(String title) {
+        setTitle(title);
+        setLocation(200, 100);
+        setSize(500, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	setJMenuBar(menuBar);
+	createAlgorithmMenu();
+	setVisible(true);
+	}
 	/*to do the algorithm first create Kmeans object
 	 * then load the database using method loadData on this object, 
 	 * passing HashTable<String, Boolean> where string is state abriviation and boolean is value indicating if state has been selected
@@ -20,6 +28,27 @@ public class KmeansGUI {
 	 * then in the end call method  getStatistics() statistics to get returned information to display regarding EV and IV
 	 * tu run again set the object to null and do above steps once again
 	 */
-	
+	 //create the alogrithm menu and adds it to the menu bar
+    private void createAlgorithmMenu() {
+        JMenuItem algorithmItem;//menu item variable that is used to add menu items to the menu
+        JMenu algorithmMenu = new JMenu("Algorithm");//declares algorithm menu 
+        MenuHandler mh = new MenuHandler();//object of menu handler class that has a method that responds to the users click of the menu item
+        
+        //initializes menu item, adds a action listener to the menu item, and add the menu item to the menu
+        algorithmItem = new JMenuItem("Load Data");
+        algorithmItem.addActionListener(mh);
+        algorithmMenu.add(algorithmItem);
+
+        //draws a line to seperate between one menu item and another
+        algorithmMenu.addSeparator();
+        
+        //initializes menu item, adds a action listener to the menu item, and add the menu item to the menu
+	algorithmItem = new JMenuItem("Run Alogrithm");
+        algorithmItem.addActionListener(mh);
+        algorithmMenu.add(algorithmItem);
+        
+        //adds the menu to the menu bar
+        menuBar.add(algorithmMenu);
+    }
 	
 }
