48b82c9f8f54012f7aa311214753ba55aca5ac3e
David <david.tzvi.rosen@gmail.com>
2016-05-22 13:57:50 -0400

    Update KmeansGUI.java

--- KmeansGUI.java
+++ KmeansGUI.java
@@ -1,7 +1,8 @@
 /*
  * KmeansGUI.java
- * This class allows user to select from dropdown menu state that they want to evaluate
- * then user can select number of k (from 2 to lets say 100)
+ * This class allows user to select from check boxes the targeted age group 
+ * then user can select from dropdown menu number of k (from 2 to lets say 100) 
+ * then user can select from dropown menu targeted age group 
  * Then user presses run button after which algorithm is run
  * User gets displayed statistics on JLabel as well as Centroids final values
  * <Optional> Then user will get displayed google maps showing geo-location of each centroid
@@ -9,4 +10,16 @@
  */
 public class KmeansGUI {
 
+	
+	
+	/*to do the algorithm first create Kmeans object
+	 * then load the database using method loadData on this object, 
+	 * passing HashTable<String, Boolean> where string is state abriviation and boolean is value indicating if state has been selected
+	 * then call method runKmeans() to run algorithm
+	 * then call method getCentroids() to obtain values of centroids (which are adjusted to their proper geo-location
+	 * then in the end call method  getStatistics() statistics to get returned information to display regarding EV and IV
+	 * tu run again set the object to null and do above steps once again
+	 */
+	
+	
 }
