dd80164139707177460fa7f523330eaf9b6d3f99
David <david.tzvi.rosen@gmail.com>
2016-05-23 21:44:46 -0400

    Update KmeansGUI.java

--- KmeansGUI.java
+++ KmeansGUI.java
@@ -8,9 +8,12 @@
  * <Optional> Then user will get displayed google maps showing geo-location of each centroid
  * you can find this useful for mapping multiple data points on a map https://gist.github.com/parth1020/4481893
  */
-public class KmeansGUI {
-
-	
+import javax.swing.*;
+public class KmeansGUI extends JFrame{
+Static JButton run = new JButton("Run");
+Static JButton find = new JButton("Find Optimal K");
+Static JButton restart = new JButton("Restart Algorithm");
+Static JPanel button = new JPanel();
 	
 	/*to do the algorithm first create Kmeans object
 	 * then load the database using method loadData on this object, 
@@ -20,6 +23,25 @@ public class KmeansGUI {
 	 * then in the end call method  getStatistics() statistics to get returned information to display regarding EV and IV
 	 * tu run again set the object to null and do above steps once again
 	 */
+	public KmeansGUI(String title) {
+        	setTitle(title);
+        	setLocation(200, 100);
+        	setSize(500, 300);
+        	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	
+		createButtonPanel();
+		add(button, BorderLayout.SOUTH);
+		setVisible(true);
+	}
+	
+	public createButtonPanel(){
+		button.add(run);
+		button.add(find);
+		button.add(restart);
+		run.setEnabled(true);
+		find.setEnable(false);
+		restart.setEnabled(false);
+		
+	}
 	
 }
