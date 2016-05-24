9da21fb308abab33530bcbb76d81f6086b9795ce
David <david.tzvi.rosen@gmail.com>
2016-05-23 22:16:46 -0400

    Update ButtonHandler.java

--- ButtonHandler.java
+++ ButtonHandler.java
@@ -1,5 +1,23 @@
 import java.awt.event.*;
 class ButtonHandler implements ActionListener{
+static Kmeans kmeans;
 
+  public ButtonHandler(){}
+  //responds to the user clicking on a button
+  public void actionPerformed(ActionEvent event) {
+    String buttonName = event.getActionCommand();//records the button item the user clicks on
+        
+      //series of if else statements to decide what happens when the user clicks on a specific button
+      if (buttonName.equals("Run")) {
+          kmeans = new Kmeans();//insert k number
+          loadData();//get states and age group
+          runKmeans();
+      } else if (menuName.equals("Find Optimal K")) {
+          //insert method here
+    	} else if (menuName.equals("New Evaluation")){
+    	    //set something to null
+    	    runKmeans();
+    	}
+    }  
 
 }
